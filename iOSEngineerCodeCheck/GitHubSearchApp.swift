c62f07e091634bf0c1ba16a7bce148ada3665eb4
yamadataiyou <tai09to06y@gmail.com>
2025-08-26 02:12:30 +0900

    AppDelegateとSceneDelegateを削除し、SwiftUIの@mainを導入 (#7)

---
+++ iOSEngineerCodeCheck/GitHubSearchApp.swift
@@ -0,0 +1,19 @@
+//
+//  GitHubSearchApp.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 山田大陽 on 2025/08/26.
+//  Copyright © 2025 YUMEMI Inc. All rights reserved.
+//
+
+
+import SwiftUI
+
+@main
+struct GitHubSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
